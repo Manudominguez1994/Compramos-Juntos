81dd67efdca7d97f92ec3ad23d93acd761e061d1
Manudominguez1994 <manudomnav1994@gmail.com>
2024-03-13 21:26:31 +0100

    model user finish

--- back-end/models/User.model.js
+++ back-end/models/User.model.js
@@ -13,7 +13,25 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, 'Password is required.']
-    }
+    },
+    name:{
+      type: String,
+      required: true,
+    },
+    imagen:{
+      type: String,
+      default: "public/profile-picture-default-png.png"
+    },
+    dateborn: {
+      type: Date
+    },
+    ratio: {
+      type: Number,
+      default: 0,
+      max: 5,
+      min: 1
+    },
+    coordinates: [Number]
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
